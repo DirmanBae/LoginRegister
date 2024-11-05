3878629072a954c67f54a29478557193e0dcf96c
Dirman <dirman6829@gmail.com>
2024-11-05 20:39:32 +0700

    membuat login page

---
+++ app/src/main/java/com/example/modul3_dirman/LoginPage.java
@@ -0,0 +1,33 @@
+package com.example.modul3_dirman;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button kembali = findViewById(R.id.btnKembali);
+
+
+        kembali.setOnClickListener(v -> {
+            Intent i = new Intent(this,MainActivity.class);
+            startActivity(i);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/modul3_dirman/registerPage.java
@@ -0,0 +1,36 @@
+package com.example.modul3_dirman;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class registerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnRegis = findViewById(R.id.btnRegister);
+        Button kembali = findViewById(R.id.btnKembali);
+
+
+                kembali.setOnClickListener(v -> {
+                    Intent i = new Intent(this,MainActivity.class);
+                    startActivity(i);
+                });
+
+    }
+}
